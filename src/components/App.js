0590803ed2629677bcf67150b531571e4ad3c72c
aubrey whitford <aubreya.whitford@gmail.com>
2025-01-22 12:51:38 -0500

    Completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data));
+  }, []);
+
+  const addQuestion = (newQuestion) => {
+    setQuestions([...questions, newQuestion]);
+  };
+
+  const deleteQuestion = (id) => {
+    setQuestions(questions.filter(question => question.id !== id));
+  };
+
+  const updateQuestion = (updatedQuestion) => {
+    setQuestions(questions.map(q => q.id === updatedQuestion.id ? updatedQuestion : q));
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm addQuestion={addQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={deleteQuestion}
+          onUpdateQuestion={updateQuestion}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,14 @@
 import React from "react";
-
-function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
-}
+import QuestionItem from "./QuestionItem";
+function QuestionList({ questions, onDeleteQuestion, onUpdateQuestion }) { 
+  return ( 
+  <section> 
+    <h1>Quiz Questions</h1> 
+    <ul> {questions.map((question) => ( 
+      <QuestionItem key={question.id} question={question} 
+      onDeleteQuestion={onDeleteQuestion} 
+      onUpdateQuestion={onUpdateQuestion} /> ))} 
+      </ul> 
+      </section> ); }
 
 export default QuestionList;
